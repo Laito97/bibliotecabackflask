f32243f6bac9ebff8ff5a37586691842ea82f3d5
Laito97 <kiragood666@gmail.com>
2025-07-07 19:32:51 -0500

    sa

--- app/config.py
+++ app/config.py
@@ -8,6 +8,6 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
-        f"@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
+        f"@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
